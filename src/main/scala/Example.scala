89a78a72e425bfd84bb5b27f6f00c2e9bc12f61c
Nathan Hamblen <nathan@technically.us>
2011-03-26 12:20:53 -0400

    scraper thread

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -4,8 +4,9 @@ import unfiltered.request._
 import unfiltered.response._
 import org.jboss.netty.channel.{Channel,ChannelFuture,ChannelFutureListener}
 import org.jboss.netty.channel.group.{DefaultChannelGroup, ChannelGroupFuture}
-import org.jboss.netty.handler.codec.http.{HttpHeaders,
+import org.jboss.netty.handler.codec.http.{HttpHeaders, DefaultHttpChunk,
                                            DefaultHttpChunkTrailer}
+import org.jboss.netty.buffer.ChannelBuffers
 
 
 /** unfiltered plan */
@@ -39,10 +40,25 @@ object App {
 /** embedded server */
 object Server {
   def main(args: Array[String]) {
-    println("starting unfiltered app at localhost on port %s" format 8080)
+    val scraper = new Thread {
+      private var plsfinish = false
+      def finish() { plsfinish = true }
+      override def run() {
+        while (!plsfinish) {
+          println("writing")
+          App.clients.write(new DefaultHttpChunk(
+            ChannelBuffers.copiedBuffer("".getBytes("utf-8"))
+          ))
+          Thread.sleep(5000)
+        }
+      }
+    }
+    scraper.start()
     unfiltered.netty.Http(8080)
       .handler(new App)
       .beforeStop {
+        scraper.finish()
+        scraper.join()
         App.clients.write(new DefaultHttpChunkTrailer).awaitUninterruptibly()
       }
       .run()
