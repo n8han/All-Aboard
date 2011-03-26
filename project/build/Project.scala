f3270bc3b102ad6f4727392295df5020a8d36131
Nathan Hamblen <nathan@technically.us>
2011-03-26 12:05:55 -0400

    a server that compiles

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val uf_version = "0.3.1"
+  val uf_version = "0.3.2"
   
   // unfiltered
   lazy val ufj = "net.databinder" %% "unfiltered-netty" % uf_version
@@ -16,7 +16,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   // logging
   val javaNetRepo = "Java.net Repository for Maven" at "http://download.java.net/maven/2"
   val newReleaseToolsRepository = ScalaToolsSnapshots
-  val avsl = "org.clapper" %% "avsl" % "0.3"
+//  val avsl = "org.clapper" %% "avsl" % "0.3"
 
   lazy val repo = "jboss repo" at 
     "http://repository.jboss.org/nexus/content/groups/public-jboss/"
--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -2,60 +2,49 @@ package com.example
 
 import unfiltered.request._
 import unfiltered.response._
+import org.jboss.netty.channel.{Channel,ChannelFuture,ChannelFutureListener}
+import org.jboss.netty.channel.group.{DefaultChannelGroup, ChannelGroupFuture}
+import org.jboss.netty.handler.codec.http.{HttpHeaders,
+                                           DefaultHttpChunkTrailer}
 
-import org.clapper.avsl.Logger
 
 /** unfiltered plan */
-class App extends unfiltered.netty.cycle.Plan {
+class App extends unfiltered.netty.channel.Plan {
   import QParams._
   
-  val logger = Logger(classOf[App])
-  
+  implicit def block2listener(block: () => Unit) =
+    new ChannelFutureListener {
+      def operationComplete(future: ChannelFuture) { block() }
+    }
+  val ChunkedJson =
+    unfiltered.response.Connection(HttpHeaders.Values.CLOSE) ~>
+    TransferEncoding(HttpHeaders.Values.CHUNKED) ~>
+    JsonContent
+
   def intent = {
-    case GET(Path(p)) => 
-      logger.debug("GET %s" format p)
-      view(Map.empty)(<p> What say you? </p>)
-    case POST(Path(p) & Params(params)) =>
-      logger.debug("POST %s" format p)
-      val vw = view(params)_
-      val expected = for { 
-        int <- lookup("int") is
-          int { s => "'%s' is not an integer".format(s) } is
-          required("missing int")
-        word <- lookup("palindrome") is
-          trimmed is 
-          nonempty("Palindrome is empty") is
-          pred(palindrome, { s =>
-            "%s is not a palindrome".format(s)
-          }) is
-          required("missing palindrome")
-      } yield vw(<p>Yup. { int.get } is an integer and { word.get } is a palindrome. </p>)
-      expected(params) orFail { fails =>
-        vw(<ul> { fails.map { f => <li>{f.error} </li> } } </ul>)
+    case req @ GET(Path(Seg("nyp" :: Nil))) =>
+      val ch = req.underlying.event.getChannel
+      val initial = req.underlying.defaultResponse(ChunkedJson)
+      ch.write(initial).addListener { () =>
+        App.clients.add(ch)
+        ()
       }
   }
-  def palindrome(s: String) = s.toLowerCase.reverse == s.toLowerCase
-  def view(params: Map[String, Seq[String]])(body: scala.xml.NodeSeq) = {
-    def p(k: String) = params.get(k).flatMap { _.headOption } getOrElse("")
-    Html(
-     <html><body>
-       { body }
-       <form method="POST">
-         Integer <input name="int" value={ p("int") } ></input>
-         Palindrome <input name="palindrome" value={ p("palindrome") } />
-         <input type="submit" />
-       </form>
-     </body></html>
-   )
-  }
+}
+
+object App {
+  val clients = new DefaultChannelGroup
 }
 
 /** embedded server */
 object Server {
-  val logger = Logger(Server.getClass)
-  
   def main(args: Array[String]) {
-    logger.info("starting unfiltered app at localhost on port %s" format 8080)
-    unfiltered.netty.Http(8080).handler(new App).run
+    println("starting unfiltered app at localhost on port %s" format 8080)
+    unfiltered.netty.Http(8080)
+      .handler(new App)
+      .beforeStop {
+        App.clients.write(new DefaultHttpChunkTrailer).awaitUninterruptibly()
+      }
+      .run()
   }
 }
